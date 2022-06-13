c3bd8be0591549e7bda12effbff606ef25abc7f6
masatoshi kusano <uppleate@cordiary.net>
2022-06-13 16:01:57 +0900

    関数定義の修正、及びHTMLファイルの追加

--- kadai①-3.js
+++ kadai①-3.js
@@ -1,23 +1,22 @@
-const taskHead = 
+const title = 
 `===========================
 現在持っているタスクの一覧
 ===========================`;
-
 const taskList = ['掃除', '買い物', '散歩'];
 
-function newTask(addTask) {
-  taskList.push(addTask);
-  console.log(taskHead);
+function display_taskList() {
+  console.log(title);
   for (let i = 0; i < taskList.length; i++) {
     console.log(`${i}：${taskList[i]}`);
   }
 }
 
-console.log(taskHead);
-for (let i = 0; i < taskList.length; i++) {
-  console.log(`${i}：${taskList[i]}`);
+const newTask = (addTask) => {
+  taskList.push(addTask);
+  display_taskList();
 }
 
+display_taskList();
 const inputTask = prompt('タスクを入力して下さい');
 newTask(inputTask);
 const selectTask = prompt('「確認」「追加」「削除」「終了」の４つのいずれかを入力してください');
