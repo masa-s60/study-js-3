994901de5089b666a322f2b94aab1b8fa4401d27
masatoshi kusano <uppleate@cordiary.net>
2022-06-19 18:37:49 +0900

    コメントの削除・コールバック関数に第２引数を追加

--- kadai①-3.js
+++ kadai①-3.js
@@ -3,20 +3,13 @@ const title =
 現在持っているタスクの一覧
 ===========================`;
 const taskList = ['掃除', '買い物', '散歩'];
-let i = 0;
 
 const displayTaskList = () => {
   console.log(title);
-  taskList.forEach((value) => {
-    console.log(`${i}：${value}`);
-    i++;
+  taskList.forEach((value, index) => {
+    console.log(`${index}：${value}`);
   });
-  i = 0;
 }
-/* 
-「上記のforEach文、ロジックや使い方など問題ないでしょうか。」
-このようにコメントを使用した質問方法、という事で間違いないでしょうか？
-*/
 
 const newTask = (addTask) => {
   taskList.push(addTask);
